c6354ef6f699eddaec6898bbe0383e41fd6bc167
Ross <rosshamey@gmail.com>
2023-02-10 11:48:47 -0500

    finish eggplant bot

--- eggplantSacrifice.py
+++ eggplantSacrifice.py
@@ -22,14 +22,14 @@ plant = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/
 time.sleep(1)
 
 while True:
-    for i in range(0, 2):
+    for i in range(0, 20):
         time.sleep(62)
         harvest = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'Harvest All')]"))).click()
         time.sleep(2)
         plant = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'Plant All')]"))).click()
 
     harvesting = True
-    
+
     while harvesting == True:
         time.sleep(1)
         home = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='item-title'][contains(text(), 'Home')]"))).click()
@@ -71,7 +71,7 @@ while True:
         farm = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'My Farm')]"))).click()
         time.sleep(2)
 
-        for i in range(0, 2):
+        for i in range(0, 20):
             if i == 0:
                 time.sleep(35)
                 harvest = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'Harvest All')]"))).click()
