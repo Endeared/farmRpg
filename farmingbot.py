5e16eb89c121e80ba784da36ae78a02c52a9f6c2
Ross <rosshamey@gmail.com>
2023-02-09 23:04:25 -0500

    Update farmingbot.py

--- farmingbot.py
+++ farmingbot.py
@@ -25,5 +25,13 @@ time.sleep(5)
 plant = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'Plant All')]")))
 plant.click()
 
+ready = False
+
+time.sleep(5)
+while ready == False:
+    progress = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "c-progress-bar-fill pb11")))
+    print(progress)
+    time.sleep(5)
+
 while True:
     pass
