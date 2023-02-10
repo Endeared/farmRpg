2b31a9b1d3862a241dfaaead4b189f7bc810ae6a
Ross <rosshamey@gmail.com>
2023-02-09 23:10:45 -0500

    Update farmingbot.py

--- farmingbot.py
+++ farmingbot.py
@@ -20,18 +20,19 @@ farm.click()
 time.sleep(5)
 harvest = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'Harvest All')]")))
 harvest.click()
-
 time.sleep(5)
 plant = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'Plant All')]")))
 plant.click()
 
-ready = False
+harvesting = True
+while harvesting == True:
+    time.sleep(65)
 
-time.sleep(5)
-while ready == False:
-    progress = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "c-progress-bar-fill pb11")))
-    print(progress)
+    harvest = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'Harvest All')]")))
+    harvest.click()
     time.sleep(5)
+    plant = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'Plant All')]")))
+    plant.click()
 
 while True:
     pass
