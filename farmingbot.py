368b8c73e2ce495a408130ad364d7ea1cc68e946
Ross <rosshamey@gmail.com>
2023-02-09 22:41:28 -0500

    Update farmingbot.py

--- farmingbot.py
+++ farmingbot.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
   
 driver = webdriver.Chrome()
-driver.get("https://farmrpg.com/index.php#!/xfarm.php?id=181768")
-# element = driver.find_element(By.NAME, '')
+driver.get("https://farmrpg.com/index.php#!/login.php")
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, "username"))).send_keys('Leonhart')
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, "password"))).send_keys('jt64agc2')
+login = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'login_sub')))
 
-element.click()
+login.click()
 while True:
     pass
