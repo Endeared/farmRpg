181c0276e92f3596363411e9a42d5becda280b88
Ross <rosshamey@gmail.com>
2023-02-09 22:58:02 -0500

    Update farmingbot.py

--- farmingbot.py
+++ farmingbot.py
@@ -6,6 +6,7 @@ import time
   
 driver = webdriver.Chrome()
 driver.get("https://farmrpg.com/index.php#!/login.php")
+driver.maximize_window()
 
 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, "username"))).send_keys('Leonhart')
 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, "password"))).send_keys('jt64agc2')
@@ -16,11 +17,13 @@ login.click()
 farm = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'My Farm')]")))
 farm.click()
 
-# harvest = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'Harvest All')]")))
-# harvest.click()
+time.sleep(5)
+harvest = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'Harvest All')]")))
+harvest.click()
 
-# plant = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'Plant All Selected')]")))
-# plant.click()
+time.sleep(5)
+plant = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'Plant All')]")))
+plant.click()
 
 while True:
     pass
