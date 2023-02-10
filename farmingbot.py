deacb572cd7b01e49337f8429f08ae24b84b33f2
Ross <rosshamey@gmail.com>
2023-02-09 22:24:02 -0500

    first commit

---
+++ farmingbot.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+  
+driver = webdriver.Chrome()
+driver.get("https://farmrpg.com/index.php#!/xfarm.php?id=181768")
+# element = driver.find_element(By.NAME, '')
+
+element.click()
+while True:
+    pass
