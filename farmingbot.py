3e010b3d39593e8efdb44d7e5112d0099e1883a2
Ross <rosshamey@gmail.com>
2023-02-09 22:50:44 -0500

    Update farmingbot.py

--- farmingbot.py
+++ farmingbot.py
@@ -6,10 +6,21 @@ import time
   
 driver = webdriver.Chrome()
 driver.get("https://farmrpg.com/index.php#!/login.php")
+
 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, "username"))).send_keys('Leonhart')
 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, "password"))).send_keys('jt64agc2')
-login = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'login_sub')))
 
+login = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'login_sub')))
 login.click()
+
+farm = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'My Farm')]")))
+farm.click()
+
+# harvest = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'Harvest All')]")))
+# harvest.click()
+
+# plant = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'Plant All Selected')]")))
+# plant.click()
+
 while True:
     pass
