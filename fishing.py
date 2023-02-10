1b8535dd5234b78558feb642a2e585f75a0209d0
Ross <rosshamey@gmail.com>
2023-02-10 17:46:32 -0500

    Update fishing.py

--- fishing.py
+++ fishing.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 import time
+import random
   
 driver = webdriver.Chrome()
 actions = ActionChains(driver)
@@ -16,12 +17,42 @@ WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, "password")
 login = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'login_sub'))).click()
 time.sleep(0.5)
 fishing = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='item-title'][contains(text(), 'Go Fishing')]"))).click()
-time.sleep(1)
-driver.execute_script("var scrollingElement = (document.scrollingElement || document.body);scrollingElement.scrollTop = scrollingElement.scrollHeight;")
 time.sleep(0.5)
-fishing = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='item-title'][contains(text(), 'Pirate')]"))).click()
+# fishing = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='item-title'][contains(text(), 'Pirate')]"))).click()
+driver.get('https://farmrpg.com/#!/fishing.php?id=11')
 
-# while True:
-#     time.sleep(1)
-#     fish = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='fishcaught finalcatch1']"))).click()
-
+while True:
+    hooked = False
+    while hooked == False:
+        try:
+            fish = driver.find_element(By.CSS_SELECTOR, "img[src='/img/items/fish.png'][style*='display: inline']")
+            if bool(fish) == True:
+                time.sleep(round(random.uniform(0.1, 0.2), 3))
+                fish.click()
+                try:
+                    hook = driver.find_element(By.XPATH, "//div[@class='fishcaught finalcatch1']")
+                    if bool(hook) == True:
+                        hooked = True
+                        break
+                except:
+                    continue
+        except:
+            continue
+    while hooked == True:
+        try:
+            hook = driver.find_element(By.XPATH, "//div[@class='fishcaught finalcatch1']")
+            if bool(hook) == True:
+                time.sleep(round(random.uniform(0.1, 0.2), 3))
+                hook.click()
+                try:
+                    hook = driver.find_element(By.XPATH, "//div[@class='fishcaught finalcatch1']")
+                    if bool(hook) == False:
+                        hooked = False
+                        break
+                except:
+                    continue
+            elif bool(hook) == False:
+                hooked = False
+                break
+        except:
+            continue
