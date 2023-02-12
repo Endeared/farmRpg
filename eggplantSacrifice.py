1a4a85264773ac3398f603762c0301bf314664e0
Ross <rosshamey@gmail.com>
2023-02-12 10:00:15 -0500

    Update eggplantSacrifice.py

--- eggplantSacrifice.py
+++ eggplantSacrifice.py
@@ -22,7 +22,7 @@ plant = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/
 time.sleep(1)
 
 while True:
-    for i in range(0, 20):
+    for i in range(0, 30):
         time.sleep(62)
         harvest = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'Harvest All')]"))).click()
         time.sleep(2)
@@ -72,7 +72,7 @@ while True:
         farm = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'My Farm')]"))).click()
         time.sleep(2)
 
-        for i in range(0, 20):
+        for i in range(0, 30):
             if i == 0:
                 time.sleep(35)
                 harvest = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'Harvest All')]"))).click()
