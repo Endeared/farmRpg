35aebb8206ecd76f6789eba32cb87b343302cf90
Ross <rosshamey@gmail.com>
2023-02-13 11:42:23 -0500

    Update tool.py

--- tool.py
+++ tool.py
@@ -1,6 +1,16 @@
 options = ['Carrot Farm (carrot)',
-        'Eggplant Sacrifice (eggplant)', 'fishing', 'pepper', 'potato']
+        'Eggplant Sacrifice (eggplant)', 
+        'Fishing Bot (fishing)', 
+        'Pepper Farm (pepper)', 
+        'Potato Sacrifice (potato)']
 import pyfiglet
 
 print(pyfiglet.figlet_format('FarmRPG Tool'))
+print('Welcome to FarmRPG Tool. Below are your options:')
+for option in options:
+    print(option)
+choice = input('Please select one of the above options, for example: "pepper"\n').lower()
 
+if choice == 'carrot':
+    execfile('carrotFarm.py')
+elif choice == 
