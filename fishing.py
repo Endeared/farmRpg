4be583e49f250318e90a866be67aec3d98d00a1d
Ross <rosshamey@gmail.com>
2023-02-13 21:57:27 -0500

    Update fishing.py

--- fishing.py
+++ fishing.py
@@ -20,28 +20,10 @@ fishing = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, 
 time.sleep(0.5)
 # fishing = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='item-title'][contains(text(), 'Pirate')]"))).click()
 driver.get('https://farmrpg.com/#!/fishing.php?id=11')
-time.sleep(2)
-home = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='item-title'][contains(text(), 'Home')]"))).click()
-time.sleep(2)
-town = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='item-title'][contains(text(), 'Go into Town')]"))).click()
-time.sleep(2)
-store = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='item-title'][contains(text(), 'Country Store')]"))).click()
-time.sleep(2)
-max = driver.find_element(By.XPATH, "//button[@class='maxqty cmaxbtn'][@data-id='18'][contains(text(), '+MAX')]")
-time.sleep(0.1)
-driver.execute_script("arguments[0].scrollIntoView(true);", max)
 time.sleep(1)
-max.click()
-buy = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//span[@class='price18']"))).click()
-time.sleep(1)
-yes = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='actions-modal-button'][contains(text(), 'Yes')]"))).click()
-time.sleep(1)
-ok = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//span[@class='modal-button'][contains(text(), 'OK')]"))).click()
-time.sleep(1)
-
-fishing = True
 
 while True:
+    fishing = True
     while fishing == True:
         try:
             fish = driver.find_element(By.CSS_SELECTOR, "img[src='/img/items/fish.png'][style*='display: inline']")
@@ -56,11 +38,10 @@ while True:
         try:
             outOfWorms = driver.find_element(By.XPATH, "//strong[text()='0']")
         except:
+            fishing = False
             break
     
-    time.sleep(0.5)
-    sellAll = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//a[@class='button btngreenalt sellallfishbtnnc']"))).click()
-    time.sleep(1)
+    time.sleep(2)
     home = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='item-title'][contains(text(), 'Home')]"))).click()
     time.sleep(2)
     town = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='item-title'][contains(text(), 'Go into Town')]"))).click()
@@ -70,5 +51,19 @@ while True:
     max = driver.find_element(By.XPATH, "//button[@class='maxqty cmaxbtn'][@data-id='18'][contains(text(), '+MAX')]")
     time.sleep(0.1)
     driver.execute_script("arguments[0].scrollIntoView(true);", max)
-    time.sleep(1)
+    time.sleep(2)
     max.click()
+    buy = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//span[@class='price18']"))).click()
+    time.sleep(1)
+    yes = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='actions-modal-button'][contains(text(), 'Yes')]"))).click()
+    time.sleep(1)
+    ok = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//span[@class='modal-button'][contains(text(), 'OK')]"))).click()
+    time.sleep(1)
+    home = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='item-title'][contains(text(), 'Home')]"))).click()
+    time.sleep(2)
+    fishing = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='item-title'][contains(text(), 'Go Fishing')]"))).click()
+    time.sleep(2)
+    driver.get('https://farmrpg.com/#!/fishing.php?id=11')
+    time.sleep(2)
+
+    
